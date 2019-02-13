1dee626a5637e3160c363f152020cf39e8a672da
Terence1991 <cplterencehawes@gmail.com>
2019-02-13 20:28:52 +0000

    project finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts())
+
+  
+   _.chain(getState().posts)
+   .map('userId')
+   .uniq()
+   .forEach((id) => {
+     fetchUser(id)
+      })
+      .value()
 }
 
 
